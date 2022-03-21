01df1cecffa79e2403fb8a81f5b6ad25bd5f7801
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-21 11:47:37 +0900

    Add middleware

--- src/models/user.js
+++ src/models/user.js
@@ -49,14 +49,22 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-// // userSchema.methods.generateAuthToken = async function () {
-//     const user = this
-//     const token = jwt.sign({ _id: user._id }, 'thisismynewcourse')
-    // user.tokens = user.tokens.concat({ token })
-    // await user.save()
-        
-//     return token
-// }
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id }, 'thisismynewcourse')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 const jwt = require('jsonwebtoken')
 
@@ -9,10 +10,7 @@ router.post('/users', async (req, res) => {
     
     try {
         await user.save()
-        // const token = await User.generateAuthToken()
-        const token = jwt.sign({ _id: user._id }, 'thisismynewcourse')
-        user.tokens = user.tokens.concat({ token })
-        await user.save()
+        const token = await User.generateAuthToken()
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -23,23 +21,39 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        // const token = await User.generateAuthToken()
-        const token = jwt.sign({ _id: user._id }, 'thisismynewcourse')
-        user.tokens = user.tokens.concat({ token })
-        await user.save()
-
-        res.send({user, token})
-        // res.status(200).send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token})
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
 
     try {
-        const users = await User.find({})
-        res.send(users)
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
