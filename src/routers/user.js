28b6793c97dd52d0719b547acc394fd380ec8fb9
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-21 13:48:16 +0900

    Set auth to tasks

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,7 +10,7 @@ router.post('/users', async (req, res) => {
     
     try {
         await user.save()
-        const token = await User.generateAuthToken()
+        const token = await user.generateAuthToken()
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -77,7 +77,7 @@ router.get('/users/:id', async (req, res) => {
 
 });
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const _id = req.params.id
 
     const updates = Object.keys(req.body)
@@ -92,17 +92,11 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
 
-        const user = await User.findById(_id)
-
-        updates.forEach((update) => user[update] = req.body[update])
+        updates.forEach((update) => req.user[update] = req.body[update])
         
-        await user.save()
+        await req.user.save()
 
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        res.send(req.user)
 
     } catch (e) {
 
@@ -111,16 +105,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
 
     try {
-
-        const user = await User.findByIdAndDelete(req.params.id)
-        
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
