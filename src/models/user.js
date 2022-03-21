05a6e2be79043501e7b3722c07596c20883e4553
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-21 14:07:38 +0900

    Add timestamps

--- src/models/user.js
+++ src/models/user.js
@@ -1,45 +1,46 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken') 
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
         type: String,
         unique: true,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid');
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7,
-		trim: true,
-		validate(value) {
-			if (value.toLowerCase().includes('password')) {
-				throw new Error('Password cannot contain "password"');
-			}
-		},
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be a positive number');
-			}
-		},
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            }
+        },
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        },
     },
     tokens: [{
         token: {
@@ -47,7 +48,11 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
-})
+},
+    {
+        timestamps: true
+    }
+)
 
 userSchema.virtual('tasks', {
     ref: 'Task',
@@ -96,6 +101,13 @@ userSchema.pre('save', async function (next) {
         user.password = await bcrypt.hash(user.password, 8)
     }
 
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id})
     next()
 })
 
