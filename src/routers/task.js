0e1fb2add88911bd9b448f1716bc3809a50b6c87
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-27 16:57:39 +0900

    Create task test

--- src/routers/task.js
+++ src/routers/task.js
@@ -96,7 +96,7 @@ router.delete('/tasks/:id', auth, async (req, res) => {
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id})
 
         if (!task) {
-            res.status(404).send()
+            return res.status(404).send()
         }
         res.send(task)
     } catch (e) {
